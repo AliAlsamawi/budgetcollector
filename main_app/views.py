74ee4478f3fa37858719c9e17d331dccb78b05fc
Ali A <alsamawiali4@gmail.com>
2021-11-24 15:13:17 -0500

    fixed detail page

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from .models import Month
 
-from django.http import HttpResponse
+
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
@@ -17,19 +17,12 @@ class Months:  # Note that parens are optional if not inheriting from another cl
     self.year = year
 
 
-months = [
-  Months('March', 1000, 2021),
-  Months('apirl', 1000, 2021),
-  Months('Feb', 1000, 2021),
-  Months('Dec', 1000, 2021),
-]
+
 def months_index(request):
   months = Month.objects.all()
   return render(request, 'months/index.html', { 'months': months })
 
-def home(request):
-  return render(request, 'home.html')
 
 def months_detail(request, month_id):
   month = Month.objects.get(id=month_id)
-  return render(request, 'months/detail.html', { 'months': months })
+  return render(request, 'months/detail.html', { 'month': month })
