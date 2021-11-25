d4cec732368622c65a2b2f6508d260711a6deddc
Ali A <alsamawiali4@gmail.com>
2021-11-25 11:27:00 -0500

    second modle

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
+
 class Month(models.Model):
   name = models.CharField(max_length=100)
   year = models.CharField(max_length=100)
   budget = models.IntegerField()
+class Expenses(models.Model):
+  date = models.DateField()
+  price = models.IntegerField()
+  reason = models.CharField(max_length=100)
+  worthit = models.CharField(max_length=1)
+  recurring = models.CharField(max_length=1)
+  paid = models.CharField(max_length=1)
 
   def __str__(self):
     return self.name
