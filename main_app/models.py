0a28e9865a644ab9b6b358dc9470bb484403ad99
Ali A <alsamawiali4@gmail.com>
2021-11-26 11:51:21 -0500

    auth

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 WORTHIT = (
   ('Y', 'Yes'),
@@ -18,6 +19,7 @@ class Month(models.Model):
   name = models.CharField(max_length=100)
   year = models.CharField(max_length=100)
   budget = models.IntegerField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
