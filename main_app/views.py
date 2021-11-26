2a7c46928bf82fbf8a3802e4bc8f479153349343
Ali A <alsamawiali4@gmail.com>
2021-11-26 12:22:11 -0500

    sign up function

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Month
 from .forms import ExpenseForm
 from django.contrib.auth.views import LoginView
@@ -53,3 +55,18 @@ def add_expense(request, month_id):
     new_expense.month_id = month_id
     new_expense.save()
   return redirect('months_detail', month_id=month_id)
+
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('months_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
